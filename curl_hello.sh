8eaa0559c1f320af59844f3c499ad6e4a7314d98
advait <advait@roam7.com>
2013-06-11 16:42:10 +0800

    Removed obsolete code

--- curl_hello.sh
+++ curl_hello.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 base_url="http://localhost"
 echo "does it work?"
+echo "URL: $base_url/echo_hello.php?and=parameters" 
 echo "raw:"
 curl --silent "$base_url/echo_hello.php?and=parameters"
 result=`curl --silent "$base_url/echo_hello.php?and=parameters"`
--- setup_fpm_and_nginx_for_travis.sh
+++ setup_fpm_and_nginx_for_travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+pwd
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo apt-get -y install php5-fpm
-cp .travis_nginx.conf /etc/nginx/nginx.conf
-cat nginx-default-site.conf | sed -e "s,DOCROOT,`pwd`," | sudo tee /etc/nginx/sites-available/default > /dev/null
+cat nginx-default-site.conf | sed -e "s,DOCROOT,`pwd`," | sudo tee /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
