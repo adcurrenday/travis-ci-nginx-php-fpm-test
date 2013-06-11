aa31904a82cd1b8e21675a99fc0003495a03c8ef
advait <advait@roam7.com>
2013-06-11 17:21:23 +0800

    Add php5-fpm restart

--- setup_fpm_and_nginx_for_travis.sh
+++ setup_fpm_and_nginx_for_travis.sh
@@ -4,4 +4,5 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo apt-get -y install php5-fpm
 cat nginx-default-site.conf | sed -e "s,DOCROOT,`pwd`," | sudo tee /etc/nginx/sites-available/default
+sudo /etc/init.d/php5-fpm restart
 sudo /etc/init.d/nginx restart
