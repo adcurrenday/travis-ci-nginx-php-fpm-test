1025a1be5599fa7261744f84735c064cefd04c2f
advait <advait@roam7.com>
2013-06-11 17:51:18 +0800

    switched back to nginx conf from site conf

--- setup_fpm_and_nginx_for_travis.sh
+++ setup_fpm_and_nginx_for_travis.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-ls /var
-ls /var/run
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo apt-get -y install php5-fpm
-cat nginx-default-site.conf | sed -e "s,DOCROOT,`pwd`," | sudo tee /etc/nginx/sites-available/default
-sudo /etc/init.d/php5-fpm restart
+cat .travis_nginx.conf | sed -e "s,DOCROOT,`pwd`," | sudo tee /etc/nginx/nginx
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/php5-fpm restart
+
