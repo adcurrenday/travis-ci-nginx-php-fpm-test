d2bf6d74faf456764c22f56ccd6a04560057f687
advait <advait@roam7.com>
2013-06-11 18:02:28 +0800

    corrected setup

--- setup_fpm_and_nginx_for_travis.sh
+++ setup_fpm_and_nginx_for_travis.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo apt-get -y install php5-fpm
-cat .travis_nginx.conf | sed -e "s,DOCROOT,`pwd`," | sudo tee /etc/nginx/nginx
+cat .travis_nginx.conf | sed -e "s,DOCROOT,`pwd`," | sudo tee /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/php5-fpm restart
 
