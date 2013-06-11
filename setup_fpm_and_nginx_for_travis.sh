941e7487d74a5e6869513676656fc85532f5f368
advait <advait@roam7.com>
2013-06-11 15:48:20 +0800

    Using default site conf instead of nginx global conf. Added an install task for python dependencies.

--- setup_fpm_and_nginx_for_travis.sh
+++ setup_fpm_and_nginx_for_travis.sh
@@ -3,4 +3,5 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo apt-get -y install php5-fpm
 cp .travis_nginx.conf /etc/nginx/nginx.conf
+cat nginx-default-site.conf | sed -e "s,DOCROOT,`pwd`," | sudo tee /etc/nginx/sites-available/default > /dev/null
 sudo /etc/init.d/nginx restart
